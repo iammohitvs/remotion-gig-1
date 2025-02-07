625403078cf0b11c25093d07edef5d4e005c4fc0
Mohit V S <vsmohit94@gmail.com>
2025-02-07 23:24:45 +0530

    removed images to hardcode texts instead

--- src/Chat/ChatHeader.tsx
+++ src/Chat/ChatHeader.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import { ArrowLeft, Upload, Volume2 } from "lucide-react";
+import React from "react";
 import { Img, staticFile } from "remotion";
 
 const ChatHeader = () => {
   return (
-    <div className='py-3 bg-gray-100 w-full pt-10'>
-      <Img src={staticFile("header-image.png")} className='w-full' />
+    <div className="py-3 bg-gray-100 w-full pt-20 px-10 flex flex-row justify-between items-center">
+      <div className="flex flex-row gap-10 items-center">
+        <ArrowLeft size={60} />
+        <Img
+          src={staticFile("user-avatar.png")}
+          className="w-40 h-40 rounded-full"
+        />
+        <div className="flex flex-col gap-5 items-start">
+          <p className="font-bold text-5xl">Nate</p>
+          <div className="flex flex-row gap-3 items-center">
+            <div className="w-5 h-5 rounded-full bg-green-500" />
+            <p className="text-4xl font-semibold text-green-500">Online</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row gap-10">
+        <Volume2 size={60} />
+        <Upload size={60} />
+      </div>
     </div>
   );
-}
+};
 
-export default ChatHeader
+export default ChatHeader;
--- src/Chat/Messages.tsx
+++ src/Chat/Messages.tsx
@@ -5,32 +5,39 @@ const Messages = () => {
   const { fps, durationInFrames } = useVideoConfig();
   const frame = useCurrentFrame();
 
-  const messageOneOpacity =
-    frame < 30 ? frame / durationInFrames : 1;
+  const messageOneOpacity = frame < 30 ? frame / durationInFrames : 1;
 
   const messageTwoOpacity =
-    frame < 60 && frame > 30
-      ? frame / durationInFrames
-      : 1;
+    frame < 60 && frame > 30 ? frame / durationInFrames : 1;
 
   return (
     <div className="flex-1 w-full pt-20 flex flex-col gap-10">
       <div
-        className="w-[80%] mr-auto"
+        className="w-[80%] mr-auto ml-10"
         style={{
           opacity: messageOneOpacity,
         }}
       >
-        <Img src={staticFile("msg-1.png")} className="w-full" />
+        <div className="flex flex-row gap-10 items-end justify-start">
+          <Img
+            src={staticFile("user-avatar.png")}
+            className="w-30 h-30 rounded-full"
+          />
+          <p className="text-5xl bg-gray-200 p-14 rounded-4xl rounded-bl-none">
+            This is some sample text. This text can be edited!
+          </p>
+        </div>
       </div>
 
       <div
-        className="w-[80%] ml-auto mr-7"
+        className="w-[80%] ml-auto mr-10"
         style={{
           opacity: frame > 30 ? messageTwoOpacity : 0,
         }}
       >
-        <Img src={staticFile("msg-2.png")} className="w-full" />
+        <p className="text-5xl bg-blue-600 text-white p-14 rounded-4xl rounded-bl-none">
+          This is some sample text. This text can be edited!
+        </p>
       </div>
     </div>
   );
